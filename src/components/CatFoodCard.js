1f18b0de1646e63d61493d9debb7cf205f29a4f8
Elnura D <elnurab@gmail.com>
2022-08-26 18:30:09 +0800

    added prices to the ui

--- src/components/CatFoodCard.js
+++ src/components/CatFoodCard.js
@@ -2,17 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './CatFoodCard.module.css';
 
-function CatFoodCard({ foodImage, foodName }) {
+function CatFoodCard({
+  foodImage, foodName, foodPrice, handleClick,
+}) {
   CatFoodCard.propTypes = {
     foodImage: PropTypes.string.isRequired,
     foodName: PropTypes.string.isRequired,
+    foodPrice: PropTypes.number.isRequired,
+    handleClick: PropTypes.func.isRequired,
   };
 
   return (
     <div>
       <img className={styles.catFoodImg} src={foodImage} alt="" />
       <h4>{foodName}</h4>
-      <button type="button">Add to Cart</button>
+      <p>
+        $
+        {foodPrice}
+
+      </p>
+      <button type="button" onClick={handleClick}>Add to Cart</button>
     </div>
 
   );
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import CatFoodCard from './CatFoodCard';
 import styles from './Shop.module.css';
@@ -14,62 +14,78 @@ const foodImageSix = require('../assets/pure_beef.jpg');
 const foodImageSeven = require('../assets/pure_green_tripe.jpg');
 const foodImageEight = require('../assets/pure_turkey.jpg');
 
-const images = [{
+const foodItems = [{
 
   image: foodImageOne,
   name: 'Country Banquet Turkey',
+  price: 11.20,
   id: 1,
 },
 {
   image: foodImageTwo,
   name: 'Cat Beef Chicken',
+  price: 10.50,
   id: 2,
 },
 {
   image: foodImageThree,
   name: 'Cat Lamb Chicken',
+  price: 9.45,
   id: 3,
 },
 {
   image: foodImageFour,
   name: 'Country Lamb Chicken',
+  price: 12,
   id: 4,
 },
 {
   image: foodImageFive,
   name: 'Puppy Weaning Paste',
+  price: 9.50,
   id: 5,
 },
 {
   image: foodImageSix,
   name: 'Pure Beef',
+  price: 10,
   id: 6,
 },
 {
   image: foodImageSeven,
   name: 'Pure Green Tripe',
+  price: 9.80,
   id: 7,
 },
 {
   image: foodImageEight,
   name: 'Pure Turkey',
+  price: 12,
   id: 8,
 },
 ];
 
 function Shop() {
+  // eslint-disable-next-line no-unused-vars
+  const [cart, updateCart] = useState([]);
+  const addItemToCart = (item) => {
+    updateCart([...cart, item]);
+  };
+
   return (
     <div>
       <Link to="/"><CompanyName /></Link>
+      <Link to="/cart">Proceed to Checkout</Link>
       <div className={styles.shopContainer}>
-        {images.map((image) => (
+        {foodItems.map((item) => (
           <CatFoodCard
-            key={image.id}
-            foodImage={image.image}
-            foodName={image.name}
+            key={item.id}
+            foodImage={item.image}
+            foodName={item.name}
+            foodPrice={item.price}
+            handleClick={() => addItemToCart(item)}
           />
         ))}
-        {/* <CatFoodCard foodImage={countryBanquet} foodName="Country Banquet Turkey" /> */}
       </div>
     </div>
   );
